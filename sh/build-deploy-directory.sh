3e1dc95ab12b413942cb6229e6c2d8beca838c43
vadim-ivlev <vadim.ivlev@gmail.com>
2020-06-01 17:40:57 +0300

    fix: www/ deployed

--- sh/build-deploy-directory.sh
+++ sh/build-deploy-directory.sh
@@ -2,8 +2,10 @@
 
 rm -rf deploy/logs
 rm -rf deploy/configs
+rm -rf deploy/www
 rm -rf deploy/docker-compose.yml
 
 cp -R logs deploy/
 cp -R configs deploy/
+cp -R www deploy/
 cp -R docker-compose.yml deploy/
