377a36a17fb9c49784b86965aa379ade4a862184
vadim-ivlev <vadim.ivlev@gmail.com>
2020-06-03 03:26:33 +0300

    add: notebook1

--- sh/build-deploy-directory.sh
+++ sh/build-deploy-directory.sh
@@ -3,9 +3,13 @@
 rm -rf deploy/logs
 rm -rf deploy/configs
 rm -rf deploy/www
+rm -rf deploy/notebooks
+rm -rf Dockerfile-anaconda3
 rm -rf deploy/docker-compose.yml
 
 cp -R logs deploy/
 cp -R configs deploy/
 cp -R www deploy/
+cp -R notebooks deploy/
+cp -R Dockerfile-anaconda3 deploy/
 cp -R docker-compose.yml deploy/
