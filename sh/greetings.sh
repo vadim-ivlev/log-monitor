81e93757180bf1f27c72c1342159137e2ac65080
vadim-ivlev <vadim.ivlev@gmail.com>
2020-12-24 22:12:54 +0300

    add: shared volume for app logs

--- sh/greetings.sh
+++ sh/greetings.sh
@@ -3,10 +3,11 @@
 echo
 echo "L O G  -  M O N I T O R"
 echo
-echo "* Logstash () http://localhost:5044"
-echo "* Elasticsearch (full text search) http://localhost:9200"
-echo "* Kibana (visualize logs) http://localhost:5601"
-echo "* Cerebro (visualize cluster) http://localhost:9000"
-echo
+# echo "* Logstash () http://localhost:5044"
+# echo "* Elasticsearch (full text search) http://localhost:9200"
+# echo "* Kibana (visualize logs) http://localhost:5601"
+# echo "* Cerebro (visualize cluster) http://localhost:9000"
+# echo
 echo "* Caddy (proxy to Kibana)  http://localhost:9094"
-echo "* Caddy (proxy to Cerebro) http://localhost:9094/cerebro/#/connect"
+# echo "* Caddy (proxy to Cerebro) http://localhost:9094/cerebro/#/connect"
+echo "* Caddy (proxy to Portainer) http://localhost:9094/portainer/"
